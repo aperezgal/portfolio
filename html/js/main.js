c3b56a2763d3b5ce7dbca1ad70514f0727c3b0c9
Alberto Perez <alberto.lipe@gmail.com>
2013-10-14 19:29:55 +0200

    css changes

--- html/js/main.js
+++ html/js/main.js
@@ -48,7 +48,57 @@ window.Router = Backbone.Router.extend({
         }
         $('#content').html(this.aboutView.el);
         this.headerView.select('about-menu');
+    },
+
+
+    slidePage: function(page) {
+
+        var slideFrom,
+            self = this;
+
+        if (!this.currentPage) {
+            // If there is no current page (app just started) -> No transition: Position new page in the view port
+            $(page.el).attr('class', 'page stage-center');
+            $('#content').append(page.el);
+            this.pageHistory = [window.location.hash];
+            this.currentPage = page;
+            return;
+        }
+
+        // Cleaning up: remove old pages that were moved out of the viewport
+        $('.stage-right, .stage-left').not('#searchPage').remove();
+
+        if (page === this.searchPage) {
+            // Always apply a Back (slide from left) transition when we go back to the search page
+            slideFrom = "left";
+            $(page.el).attr('class', 'page stage-left');
+            // Reinitialize page history
+            this.pageHistory = [window.location.hash];
+        } else if (this.pageHistory.length > 1 && window.location.hash === this.pageHistory[this.pageHistory.length - 2]) {
+            // The new page is the same as the previous page -> Back transition
+            slideFrom = "left";
+            $(page.el).attr('class', 'page stage-left');
+            this.pageHistory.pop();
+        } else {
+            // Forward transition (slide from right)
+            slideFrom = "right";
+            $(page.el).attr('class', 'page stage-right');
+            this.pageHistory.push(window.location.hash);
+        }
+
+        $('#content').append(page.el);
+
+        // Wait until the new page has been added to the DOM...
+        setTimeout(function() {
+            // Slide out the current page: If new page slides from the right -> slide current page to the left, and vice versa
+            $(self.currentPage.el).attr('class', 'page transition ' + (slideFrom === "right" ? 'stage-left' : 'stage-right'));
+            // Slide in the new page
+            $(page.el).attr('class', 'page stage-center transition');
+            self.currentPage = page;
+        });
+
     }
+
 
 });
 
